05708c70c11c113933e6cd122b55ee8edef61956
renehorstmann <r.horsty@gmail.com>
2019-08-17 21:14:56 +0200

    dynarray static +impl without static

--- include/utilc/dynarray.h
+++ include/utilc/dynarray.h
@@ -19,6 +19,66 @@ typedef struct { \
     int capacity; \
     int size; \
 } name; \
+static void name ## _kill(name *self) { \
+    free(self->array); \
+    self->array = NULL; \
+    self->capacity = 0; \
+    self->size = 0; \
+} \
+static void name ## _set_capacity(name *self, int capacity) { \
+    type *new_array = (type *) realloc(self->array, capacity * sizeof(type)); \
+    if (new_array) { \
+        self->array = new_array; \
+        self->capacity = capacity; \
+        if (self->size > capacity) \
+            self->size = capacity; \
+    } \
+} \
+static void name ## _resize(name *self, int size) { \
+    if (size > self->capacity) { \
+        name ## _set_capacity(self, size * 2); \
+        if(size>self->capacity) \
+            /* alloc error */ \
+            return;\
+    } \
+    self->size = size; \
+} \
+static void name ## _push(name *self, type item) { \
+    name ## _resize(self, self->size+1); \
+    self->array[self->size-1] = item; \
+} \
+static type *name ## _append(name *self) { \
+    name ## _resize(self, self->size+1); \
+    return &self->array[self->size-1]; \
+} \
+static type name ## _pop(name *self) { \
+    if(self->size > 0) \
+        return self->array[--self->size]; \
+    type t; \
+    return t; \
+}
+
+
+
+
+/**
+ * Creates an dynamic array of type (type) with the typename name.
+ * Implementation version (without static).
+ *
+ * Construct it as empty struct ( = {};, or calloc).
+ * Kill it with <name>_kill;
+ * <name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
+ * <name>_resize sets the array size and may set capacity with size*2.
+ * <name>_push copies an item to the end of the array (copies to the last element).
+ * <name>_append creates an item at the end and returns its pointer (returns the last element).
+ * <name>_pop returns a copy of the last item and removes it from the array.
+ */
+#define DynArrayImpl(type, name) \
+typedef struct { \
+    type *array; \
+    int capacity; \
+    int size; \
+} name; \
 void name ## _kill(name *self) { \
     free(self->array); \
     self->array = NULL; \
@@ -61,7 +121,7 @@ type name ## _pop(name *self) { \
 
 /**
  * Creates an dynamic array of type (type) with the typename name.
- * Only the function prototypes will be generated. Use DynArray(type, name) in an source file.
+ * Only the function prototypes will be generated. Use DynArrayImpl(type, name) in an source file.
  *
  * Construct it as empty struct ( = {};, or calloc).
  * Kill it with <name>_kill;
@@ -103,6 +163,54 @@ typedef struct { \
     int capacity; \
     int size; \
 } name; \
+static void name ## _kill(name *self) { \
+    free(self->array); \
+    self->array = NULL; \
+    self->capacity = 0; \
+    self->size = 0; \
+} \
+static void name ## _set_capacity(name *self, int capacity) { \
+    type *new_array = (type *) realloc(self->array, capacity * sizeof(type)); \
+    if (new_array) { \
+        self->array = new_array; \
+        self->capacity = capacity; \
+        if (self->size > capacity) \
+            self->size = capacity; \
+    } \
+} \
+static void name ## _resize(name *self, int size) { \
+    if (size > self->capacity) { \
+        name ## _set_capacity(self, size * 2); \
+        if(size>self->capacity) \
+            /* alloc error */ \
+            return;\
+    } \
+    self->size = size; \
+} \
+static type *name ## _append(name *self) { \
+    name ## _resize(self, self->size+1); \
+    return &self->array[self->size-1]; \
+}
+
+
+
+/**
+ * Creates an dynamic array of type (type) with the typename name.
+ * This version dont uses copies (push and pop). For use with array types like int[2].
+ * Implementation version without static.
+ *
+ * Construct it as empty struct ( = {};, or calloc).
+ * Kill it with <name>_kill;
+ * <name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
+ * <name>_resize sets the array size and may set capacity with size*2.
+ * <name>_append creates an item at the end and returns its pointer (returns the last element).
+ */
+#define DynArrayWithoutCopyImpl(type, name) \
+typedef struct { \
+    type *array; \
+    int capacity; \
+    int size; \
+} name; \
 void name ## _kill(name *self) { \
     free(self->array); \
     self->array = NULL; \
@@ -136,7 +244,7 @@ type *name ## _append(name *self) { \
 /**
  * Creates an dynamic array of type (type) with the typename name.
  * This version dont uses copies (push and pop). For use with array types like int[2].
- * Only the function prototypes will be generated. Use DynArray(type, name) in an source file.
+ * Only the function prototypes will be generated. Use DynArrayWithoutCopyImpl(type, name) in an source file.
  *
  * Construct it as empty struct ( = {};, or calloc).
  * Kill it with <name>_kill;
