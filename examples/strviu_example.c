acf0be6b13632c27eaa36d0f694128f13560f76e
renehorstmann <r.horsty@gmail.com>
2019-11-25 11:47:29 +0100

    strviu eat/parse ints/floats/... as ascii/bin_le/bin_be

--- examples/strviu_example.c
+++ examples/strviu_example.c
@@ -38,6 +38,23 @@ int main() {
     printf("ptr_fun has %d *\n", ptr_cnt);
 
 
+    viu = ToStrViu("12345 12.34");
+    int32_t parsed_int32;
+    viu = sv_eat_int32_ascii(viu, &parsed_int32);
+    float parsed_float;
+    viu = sv_eat_float_ascii(viu, &parsed_float);
+    printf("parsed is %d and %f, remaining viu len is %zu\n", parsed_int32, parsed_float, sv_length(viu));
+
+    double binary_test = 456.789;
+    viu = (strviu) {(char *) &binary_test, (char *) &binary_test+8};
+    double parsed_double;
+    if(sv__helper_system_is_binary_little_endian_())
+        viu = sv_eat_double_binary_le(viu, &parsed_double);
+    else
+        viu = sv_eat_double_binary_be(viu, &parsed_double);
+    printf("parsed binary is %f, remaining viu len is %zu\n", parsed_double, sv_length(viu));
+
+
     // program will print:
     //start: < 	  Hallo Welt
     //  >
@@ -50,4 +67,6 @@ int main() {
     //parameter[1] <char *dst>
     //parameter[2] <int n)>
     //ptr_fun has 4 *
+    //parsed is 12345 and 12.340000, remaining viu len is 0
+    //parsed binary is 456.789000, remaining viu len is 0
 }
