7892dacd6da0c2b017d1bb356a6566de5fe95b9e
renehorstmann <r.horsty@gmail.com>
2020-06-02 18:32:07 +0200

    strpool

--- examples/strpool_example.c
+++ examples/strpool_example.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-#include "utilc/strpool.h>
+#include "utilc/strpool.h"
 
 int main() {
 
+    char buf[128];
+    strcat_into(buf, "A", "B", "C", "DEF");
+    puts(buf);
 
+    puts(sp_reverse(buf));
+    puts(sp_iter(buf, 1, 5, 2));
+    puts(sp_iter(buf, 1, 5000, -2));
+    puts(sp_iter(buf, -4, -1, 1));
 }
